daf1424e8e088feed0af85d067f5c4c24c598981
Spencer Jones <sjonesdrums@gmail.com>
2025-01-09 23:50:07 -0600

    Add services section to program view

--- ensemble/Application/Dashboard/ProgramView.swift
+++ ensemble/Application/Dashboard/ProgramView.swift
@@ -11,6 +11,8 @@ struct ProgramView: View {
     
     @Environment(\.dismiss) var dismiss
     
+    @State private var isExpanded = false
+    
     var program: Program
     
     init(_ program: Program) {
@@ -18,28 +20,108 @@ struct ProgramView: View {
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                Button(action: {
-                    dismiss()
-                }) {
-                    Image(systemName: "xmark.circle.fill") // Or a custom close icon
-                        .font(.title2)
-                }
-                .padding() // Add some padding around the button
-            }
+        VStack(alignment: .leading) {
+            header
             Text(program.title)
-                .font(.title)
+                .font(.title2)
+                .padding(.bottom, 8)
                 .fontWeight(.bold)
+            services
+            Divider()
+            
+//            RoundedRectangle(cornerRadius: 8)
+//                .fill(.ultraThinMaterial)
+//                .overlay {
+//                    ForEach(program.services, id: \.id) { service in
+//                        VStack {
+//                            HStack {
+//                                Text(service.day)
+//                                Text(service.time)
+//                                Text(service.type)
+//                            }
+//                            HStack {
+//                                Text(service.location)
+//                                Text(service.type)
+//                            }
+//                        }
+//                    }
+//                }
             Spacer()
         }
+        .padding()
         .foregroundStyle(Color.white)
         .frame(maxWidth: .infinity)
         .background(Gradients.programLargeGradient(program.id_color))
     }
+    
+    @ViewBuilder var header: some View {
+        HStack {
+            Text(program.id)
+                .fontWeight(.semibold)
+            Spacer()
+            Button(action: {
+                dismiss()
+            }) {
+                Image(systemName: "xmark.circle") // Or a custom close icon
+                    .font(.title2)
+            }
+            .padding() // Add some padding around the button
+        }
+    }
+    
+    @ViewBuilder func sectionTitle(_ title: String) -> some View {
+        HStack {
+            Text(title)
+                .font(.title3)
+                .fontWeight(.semibold)
+                .padding(.bottom, 8)
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder var services: some View {
+        VStack(alignment: .leading) {
+            sectionTitle("Services")
+            ForEach(program.services, id: \.id) { service in
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(service.day)
+                        Text(service.time)
+                        Text(service.type)
+                    }
+                    Text(service.location)
+                    Text(service.type)
+                }
+                .padding(.bottom, 8)
+            }
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
 }
 
-//#Preview {
-//    ProgramView(Program.program1)
-//}
+struct CollapsibleSection<Content: View>: View {
+    var title: String
+    @Binding var isExpanded: Bool
+    var content: () -> Content
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Button(action: { isExpanded.toggle() }) {
+                HStack {
+                    Text(title)
+                    Spacer()
+                    Image(systemName: "chevron.down")
+                        .rotationEffect(isExpanded ? .degrees(0) : .degrees(90))
+                }
+            }
+            .font(.title3)
+            if isExpanded {
+                content()
+            }
+        }
+    }
+}
+
+#Preview {
+    ProgramView(Program.programs.first!)
+}
